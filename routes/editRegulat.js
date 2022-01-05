2d22858a56b2b0590cd05913bcadb253b7570b93
Вячеслав Братченко <office.mtdev@gmail.com>
2022-01-05 17:47:34 +0300

    edit route

--- routes/editRegulat.js
+++ routes/editRegulat.js
@@ -2,9 +2,7 @@ const Router = require('express')
 const router = Router();
 const regulatController = require('../controller/regulat.controller')
 
-router.get('/', regulatController.index);
 router.get('/:docId', regulatController.index);
-router.post('/setDocumentHeader', regulatController.setDocumentHeader);
 router.put('/updateDocumentHeader/:docId', regulatController.updateDocumentHeader);
 router.put('/setDocumentBody_1/:docId', regulatController.setDocumentBody_1);
 router.put('/setDocumentBody_2/:docId', regulatController.setDocumentBody_2);
