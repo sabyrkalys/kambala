49f265c1ddcb3654365883faf217e4a4c964e1db
sabyrkalys <sabyralievkalys@gmail.com>
2022-01-20 00:12:00 +1200

    rename dir views

---
+++ password.js
@@ -0,0 +1,8 @@
+const urlObject = {
+ //password : '',
+ url:`mongodb+srv://kalys78:678Ilohat)@cluster0.gwcvi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`
+}
+
+module.exports = urlObject;
+
+
